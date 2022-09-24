fbeb520606f5d3a93c6888fbc974b1bb35ce9e5e
Achyut Shukla <achyut.shukla.btech2020@sitpune.edu.in>
2022-09-24 23:50:10 +0530

    react router dom configured, pages and components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./styles/global.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Error404 from "./pages/Error404";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
